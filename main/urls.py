26782b59759d9c1e8e5cba2ad9877d53a150087d
Nicolás Butterfield <nicobutter@gmail.com>
2024-10-30 13:42:55 -0300

    de django a angular

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Ruta para la página de inicio
-    # path('institucional/', views.institucional_view, name='institucional'),  # Nueva ruta para la página Institucional
-    path('terminos_condiciones/', views.terminos_condiciones_view, name='terminos_condiciones'),
-    path('politica_privacidad/', views.politica_privacidad_view, name='politica_privacidad'),
-]
