ca44afa78e17044308ba93f9dc82189325d84239
Nicolás Butterfield <nicobutter@gmail.com>
2025-06-20 17:13:12 -0300

    se agregó el componente secretarias

--- gremius/src/app/app.routes.ts
+++ gremius/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './home/home.component';
 import { DelegadosAfiliadosComponent } from './delegados-afiliados/delegados-afiliados.component';
 import { LeyParitaria3022Component } from './ley-paritaria-3022/ley-paritaria-3022.component';
 import { SistemaSalarialComponent } from './sistema-salarial/sistema-salarial.component';
+import { SobreNosotrosComponent } from './sobre-nosotros/sobre-nosotros.component';
+import { SecretariasComponent } from './secretarias/secretarias.component';
 
 
 export const routes: Routes = [
@@ -11,6 +13,8 @@ export const routes: Routes = [
   { path: 'general', component: DelegadosAfiliadosComponent },
   { path: 'ley-paritaria-3022', component: LeyParitaria3022Component },
   { path: 'sistema-salarial', component: SistemaSalarialComponent },
+  { path: 'sobrenosotros', component: SobreNosotrosComponent },
+  { path: 'secretarias', component: SecretariasComponent },
   { path: '**', redirectTo: '/home' },
   { path: '**', redirectTo: '' }
 ];
---
+++ gremius/src/app/secretarias/secretarias.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secretarias',
+  standalone: true,
+  imports: [],
+  templateUrl: './secretarias.component.html',
+  styleUrl: './secretarias.component.css'
+})
+export class SecretariasComponent {
+
+}
