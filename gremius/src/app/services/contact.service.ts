25a933337e84f5a38cc252d8a9d945024c4b5843
Nicolás Butterfield <nicobutter@gmail.com>
2025-08-18 21:23:07 -0300

    hay que probar el form

--- gremius/src/app/services/contact.service.ts
+++ gremius/src/app/services/contact.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 
-
 @Injectable({ providedIn: 'root' })
 export class ContactService {
   private destinatarioSource = new BehaviorSubject<string | null>(null);
   destinatario$ = this.destinatarioSource.asObservable();
+
+  // Mapeo de "destinatario" a su endpoint de Formspree
+  private formEndpoints: Record<string, string> = {
+    'gremiojudicialesrg@gmail.com': 'https://formspree.io/f/mrblvnbg',   // Río Gallegos
+    'empleadosjudiciales3dejulio@hotmail.com': 'https://formspree.io/f/mnnzyenj' // Caleta Olivia
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -15,13 +20,17 @@ export class ContactService {
   }
 
   sendMessage(data: { name: string; email: string; message: string }, destinatario: string) {
+    const endpoint = this.formEndpoints[destinatario];
+    if (!endpoint) {
+      throw new Error('No hay un endpoint configurado para este destinatario');
+    }
+
     const formData = new FormData();
-    formData.append('destinatario', destinatario);
     formData.append('name', data.name);
     formData.append('email', data.email);
     formData.append('message', data.message);
 
-    return this.http.post('https://formspree.io/f/movaerny', formData);
+    return this.http.post(endpoint, formData);
   }
 
   resetDestinatario() {
